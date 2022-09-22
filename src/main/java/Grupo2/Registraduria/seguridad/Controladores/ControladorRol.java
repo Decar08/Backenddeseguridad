c78baab234e92961f33617f34342c01070d29855
Decar08 <deart.94@gmail.com>
2022-09-21 23:01:51 -0500

    Se agregan validaciones en administración de usuarios, colección postman y documento de pruebas

---
+++ src/main/java/Grupo2/Registraduria/seguridad/Controladores/ControladorRol.java
@@ -0,0 +1,59 @@
+package Grupo2.Registraduria.seguridad.Controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import Grupo2.Registraduria.seguridad.Modelos.Rol;
+import Grupo2.Registraduria.seguridad.Repositorios.RepositorioRol;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("administrador/roles")
+public class ControladorRol {
+    @Autowired
+    private RepositorioRol miRepositorioRol;
+
+
+    @GetMapping("")
+    public List<Rol> index(){
+        return this.miRepositorioRol.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ResponseEntity<Rol> create(@RequestBody  Rol infoRol){
+        return new ResponseEntity<>(this.miRepositorioRol.save(infoRol),HttpStatus.OK);
+    }
+    @GetMapping("{id}")
+    public Rol show(@PathVariable String id){
+        Rol rolActual=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        return rolActual;
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Rol> update(@PathVariable String id,@RequestBody  Rol infoRol){
+        Rol rolActual=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        if (rolActual!=null){
+            rolActual.setNombre(infoRol.getNombre());
+            return new ResponseEntity<>(this.miRepositorioRol.save(rolActual), HttpStatus.OK);
+        }else{
+            return  new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Rol rolActual=this.miRepositorioRol
+                .findById(id)
+                .orElse(null);
+        if (rolActual!=null){
+            this.miRepositorioRol.delete(rolActual);
+        }
+    }
+}
---
+++ src/main/java/Grupo2/Registraduria/seguridad/Repositorios/RepositorioPermisosRoles.java
@@ -0,0 +1,9 @@
+package Grupo2.Registraduria.seguridad.Repositorios;
+
+import Grupo2.Registraduria.seguridad.Modelos.PermisosRoles;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositorioPermisosRoles extends MongoRepository<PermisosRoles, String>
+{
+
+}
